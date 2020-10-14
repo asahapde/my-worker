4fd7a95bf272c3d2f0bb47f6cf04abd831dfb67e
asahapde <60831459+asahapde@users.noreply.github.com>
2020-10-13 20:40:52 -0400

    Main Assignment Complete

--- index.js
+++ index.js
@@ -40,12 +40,64 @@ class LinksTransformer {
   }
 
   async element(element) {
-    let content = "";
-    for(let i = 0; i<links.length; i++){
-      content += "<a href=\"" + links[i].url + "\">" + links[i].name + "</a>"
+    let id = element.getAttribute('id')
+
+    // Populate the links div
+    if (id == 'links') {
+      let content = ''
+      for (let i = 0; i < links.length; i++) {
+        content += '<a href="' + links[i].url + '">' + links[i].name + '</a>'
+      }
+      element.setInnerContent(content, { html: true })
     }
 
-    element.setInnerContent(content, { html: true});
+    // Remove display: none from profile div
+    if (id == 'profile') {
+      element.setAttribute('style', '')
+    }
+
+    // Set the profile image
+    if (id == 'avatar') {
+      element.setAttribute(
+        'src',
+        'https://media-exp1.licdn.com/dms/image/C4E35AQEK9ZGB5Yk3Cg/profile-framedphoto-shrink_200_200/0?e=1602723600&v=beta&t=Clc29YPhvBaSEh5Y7Fcq7zgb8JBvkXlrtXRjvRUIjmw',
+      )
+    }
+
+    // set my name
+    if (id == 'name') {
+      element.setInnerContent('asahapde', { html: true })
+    }
+
+    // Remove display: none from social div
+    if (id == 'social') {
+      element.setAttribute('style', '')
+    }
+
+    // Populate the social div
+    if (id == 'social') {
+      let svgContent =
+        '<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>LinkedIn icon</title><path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"/></svg>'
+
+      let content =
+        '<a href="' +
+        'https://www.linkedin.com/in/abdullah-sahapdeen/' +
+        '">' +
+        '<svg>' +
+        svgContent +
+        '</svg>' +
+        '</a>'
+      element.setInnerContent(content, { html: true })
+    }
+
+    // Change title to my name
+    if (element.tagName == "title") {
+      element.setInnerContent("Abdullah Sahapdeen", { html: true })
+    }
+
+    if (element.tagName == "body") {
+      element.setAttribute('class', 'bg-blue-400')
+    }
   }
 }
 
@@ -68,18 +120,19 @@ async function handleRequest(request) {
     },
   }
 
-  const rewriter = new HTMLRewriter().on(
-    'div#links',
-    new LinksTransformer(links),
-  )
+  // Create the Html rewriter
+  const rewriter = new HTMLRewriter().on('*', new LinksTransformer(links))
 
-
+  // Fetch the static url page
   const url = 'https://static-links-page.signalnerve.workers.dev'
   const response = await fetch(url, init)
+
+  // Rewrite the html with links
   const newResponse = await rewriter.transform(response)
-  
+
+  // Return the page as html
   const results = await gatherResponse(newResponse)
-  return new Response(results, init); 
+  return new Response(results, init)
 }
 
 // The event listener
